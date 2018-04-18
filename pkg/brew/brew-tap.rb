5086e84c2f4050b4068c8f305350d46b387519c2
Russell Cohen <russell.r.cohen@gmail.com>
2018-04-18 09:33:28 -0700

    Update brew-tap.rb

--- pkg/brew/brew-tap.rb
+++ pkg/brew/brew-tap.rb
@@ -1,4 +1,10 @@
-# Replace all instances of YYYY
+# 1. Place this file inside of `pkg/brew` (name isn't important). The name of the file is the name of the 
+#    formula
+# 2. Create a symlink from the root directory named `HomebrewFormula` to `pkg/brew`
+# 3. Replace all instances of YYYY
+# 4. Install with:
+#$ brew tap owner/repo https://github.com/owner/repo.git
+#$ brew install name-of-formula
 class YYYYBin < Formula
   version 'YYYY'
   desc "YYYY"
